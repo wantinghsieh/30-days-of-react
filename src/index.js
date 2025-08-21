60d9c0243c83b2e1d5a2f71235c6cc86d39733f4
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-21 14:50:41 -0400

    props

--- src/index.js
+++ src/index.js
@@ -1,106 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import asabenehImage from './images/asabeneh.jpg'
 
-const welcome = 'Welcome to 30 Days Of React Challenge'
+const welcome = 'Welcome to 30 Days Of React'
 const title = 'Getting Started React'
 const subtitle = 'JavaScript Library'
 const author = {
   firstName: 'Asabeneh',
   lastName: 'Yetayeh',
 }
-const date = 'Oct 2, 2020'
+const date = 'Oct 4, 2020'
 
-// JSX element, header
-const Header = () => (
-  <header>
-    <div className='header-wrapper'>
-      <h1>{welcome}</h1>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>
-        Instructor: {author.firstName} {author.lastName}
-      </p>
-      <small>Date: {date}</small>
-    </div>
-  </header>
-)
 
-const numOne = 3
-const numTwo = 2
-
-const result = (
-  <p>
-    {numOne} + {numTwo} = {numOne + numTwo}
-  </p>
-)
-
-const yearBorn = 1820
-const currentYear = 2020
-const age = currentYear - yearBorn
-const personAge = (
-  <p>
-    {' '}
-    {author.firstName} {author.lastName} is {age} years old
-  </p>
-)
-
-// User Card Component
-const UserCard = () => (
-  <div className='user-card'>
-    <img src={asabenehImage} alt='asabeneh image' />
-    <h2>
-      {author.firstName} {author.lastName}
-    </h2>
-  </div>
-)
-
-// JSX element, main
-const techs = ['HTML', 'CSS', 'JavaScript']
-const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
-
-// JSX element, main
-const Main = () => (
-  <main>
-    <div className='main-wrapper'>
-      <div>
+// Header Component
+const Header = (props) => {
+  console.log(props) // empty object, {}
+  return (
+    <header>
+      <div className='header-wrapper'>
+        <h1>{welcome}</h1>
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
         <p>
-          Prerequisite to get started{' '}
-          <strong>
-            <em>react.js</em>
-          </strong>
-          :
+          {author.firstName} {author.lastName}
         </p>
-        <ul>{techsFormatted}</ul>
-        {result}
-        {personAge}
+        <small>{date}</small>
       </div>
-      <UserCard />
+    </header>
+  )
+}
+
+// The App, or the parent or the container component
+// Functional Component
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
     </div>
-  </main>
-)
-
-const copyRight = '2020'
-
-// JSX element, footer
-const Footer = () => (
-  <footer>
-    <div className='footer-wrapper'>
-      <p>Copyright &copy;{copyRight}</p>
-    </div>
-  </footer>
-)
-
-// JSX element, app
-const App = () => (
-  <div className='app'>
-    <Header />
-    <Main />
-    <Footer />
-  </div>
-)
+  )
+}
 
 const rootElement = document.getElementById('root')
+
 // create a root
 const root = ReactDOM.createRoot(rootElement)
 

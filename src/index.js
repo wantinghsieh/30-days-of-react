5cb921bcf55d1493b0ebccae01e96075714ac447
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-21 11:09:48 -0400

    exercise 1

--- src/index.js
+++ src/index.js
@@ -1,98 +1,11 @@
 // index.js
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// To get the root element from the HTML document
-import asabenehImage from './images/asabeneh.jpg'
-// JSX element, header
-const welcome = 'Welcome to 30 Days Of React'
-const title = 'Getting Started React'
-const subtitle = 'JavaScript Library'
-const author = {
-  firstName: 'Asabeneh',
-  lastName: 'Yetayeh',
-}
-const date = 'Oct 2, 2020'
+import frontendTechnologies from './images/frontend_technologies.png'
 
-// JSX element, header
-const header = (
-  <header>
-    <div className='header-wrapper'>
-      <h1>{welcome}</h1>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>
-        Instructor: {author.firstName} {author.lastName}
-      </p>
-      <small>Date: {date}</small>
-    </div>
-  </header>
-)
-
-const numOne = 3
-const numTwo = 2
-
-const result = (
-  <p>
-    {numOne} + {numTwo} = {numOne + numTwo}
-  </p>
-)
-
-const yearBorn = 1820
-const currentYear = new Date().getFullYear()
-const age = currentYear - yearBorn
-const personAge = (
-  <p>
-    {' '}
-    {author.firstName} {author.lastName} is {age} years old
-  </p>
-)
-
-// JSX element, main
-const techs = ['HTML', 'CSS', 'JavaScript']
-const techsFormatted = techs.map((tech) => <li>{tech}</li>)
-
-const user = (
+const tech = (
   <div>
-    <img src={asabenehImage} alt='asabeneh image' />
-  </div>
-)
-
-// JSX element, main
-const main = (
-  <main>
-    <div className='main-wrapper'>
-      <p>
-        Prerequisite to get started{' '}
-        <strong>
-          <em>react.js</em>
-        </strong>
-        :
-      </p>
-      <ul>{techsFormatted}</ul>
-      {result}
-      {personAge}
-      {user}
-    </div>
-  </main>
-)
-
-const copyRight = 'Copyright 2020'
-
-// JSX element, footer
-const footer = (
-  <footer>
-    <div className='footer-wrapper'>
-      <p>{copyRight}</p>
-    </div>
-  </footer>
-)
-
-// JSX element, app
-const app = (
-  <div className='app'>
-    {header}
-    {main}
-    {footer}
+    <img src={frontendTechnologies} alt='frontend technologies' />
   </div>
 )
 
@@ -101,4 +14,4 @@ const rootElement = document.getElementById('root')
 const root = ReactDOM.createRoot(rootElement)
 
 // render the JSX
-root.render(app)
+root.render(tech)

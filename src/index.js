14d16e2ad6b6d0dbe9d6d72623afae166a66aaaf
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-21 11:50:16 -0400

    ex2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 // index.js
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import frontendTechnologies from './images/frontend_technologies.png'
 
-const tech = (
+
+const title = <h1>SUBSCRIBE</h1>
+const subTitle = <p>sign up with email to receive new and updates. </p>
+const firstname = <button>first name</button>;
+const lastname = <button>last name</button>;
+const email = <button>email</button>;
+const subscribe = <button>Subscribe</button>;
+const app = (
   <div>
-    <img src={frontendTechnologies} alt='frontend technologies' />
+    {title}
+    {subTitle}
+    <div style={{ display: "flex", gap: "10px" }}>
+    {firstname}
+    {lastname}
+    {email}
+    </div>
+    {subscribe}
   </div>
 )
+
 
 const rootElement = document.getElementById('root')
 // create a root
 const root = ReactDOM.createRoot(rootElement)
 
 // render the JSX
-root.render(tech)
+root.render(app)

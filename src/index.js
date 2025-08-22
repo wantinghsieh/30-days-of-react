fe38eabe5949b827e0bcd766de9e220950a7de3a
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-22 13:27:48 -0400

    state

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
+// index.js
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const Numbers = ({ numbers }) => {
-  // modifying array to array of li JSX
-  const list = numbers.map((num) => <li key={num}>{num}</li>)
-  return list
-}
-
-const App = () => {
-  const numbers = [1, 2, 3, 4, 5]
-
-  return (
-    <div className='container'>
-      <div>
-        <h1>Numbers List</h1>
-        <ul>
-          <Numbers numbers={numbers} />
-        </ul>
+class App extends React.Component {
+  // declaring state
+  state = {
+    count: 0,
+  }
+  render() {
+    // accessing the state value
+    const count = this.state.count
+    return (
+      <div className='App'>
+        <h1>{count} </h1>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
 const rootElement = document.getElementById('root')
 
 // create a root

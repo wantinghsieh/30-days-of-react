769b9ad4e47d8ed667825c879e4f4f59f4edcd32
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-22 11:33:32 -0400

    Key in mapping arrays

--- src/index.js
+++ src/index.js
@@ -1,38 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const countries = [
-  { name: 'Finland', city: 'Helsinki' },
-  { name: 'Sweden', city: 'Stockholm' },
-  { name: 'Denmark', city: 'Copenhagen' },
-  { name: 'Norway', city: 'Oslo' },
-  { name: 'Iceland', city: 'Reykjavík' },
-]
+const Numbers = ({ numbers }) => {
+  // modifying array to array of li JSX
+  const list = numbers.map((num) => <li key={num}>{num}</li>)
+  return list
+}
 
-// Country component
-const Country = ({ country: { name, city } }) => {
+const App = () => {
+  const numbers = [1, 2, 3, 4, 5]
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <small>{city}</small>
+    <div className='container'>
+      <div>
+        <h1>Numbers List</h1>
+        <ul>
+          <Numbers numbers={numbers} />
+        </ul>
+      </div>
     </div>
   )
 }
-
-// countries component
-const Countries = ({ countries }) => {
-  const countryList = countries.map((country) => <Country country={country} />)
-  return <div>{countryList}</div>
-}
-// App component
-const App = () => (
-  <div className='container'>
-    <div>
-      <h1>Countries List</h1>
-      <Countries countries={countries} />
-    </div>
-  </div>
-)
 
 const rootElement = document.getElementById('root')
 

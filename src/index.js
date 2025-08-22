f97f7bacdb5154baa6b8189d48f10e7dca528e2a
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-22 08:52:45 -0400

    props

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client'
 
 // Header Component
 const Header = (props) => {
-  console.log(props) // {welcome:'Welcome to 30 Days Of React'}
+  console.log(props)
   return (
     <header>
       <div className='header-wrapper'>
         <h1>{props.welcome}</h1>
+        <h2>{props.title}</h2>
+        <h3>{props.subtitle}</h3>
+        <p>
+          {props.firstName} {props.lastName}
+        </p>
+        <small>{props.date}</small>
       </div>
     </header>
   )
@@ -15,16 +21,20 @@ const Header = (props) => {
 
 // The App, or the parent or the container component
 // Functional Component
-const App = () => {
-  return (
-    <div className='app'>
-      <Header welcome='Welcome to 30 Days Of React' />
-    </div>
-  )
-}
+const App = () => (
+  <div className='app'>
+    <Header
+      welcome='Welcome to 30 Days Of React'
+      title='Getting Started React'
+      subtitle='JavaScript Library'
+      firstName='Asabeneh'
+      lastName='Yetayeh'
+      date='Oct 4, 2020'
+    />
+  </div>
+)
 
 const rootElement = document.getElementById('root')
-
 
 // create a root
 const root = ReactDOM.createRoot(rootElement)

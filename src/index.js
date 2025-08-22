7bbb3c52330254791f3b3c039804b4a01d0f49b9
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-22 10:00:28 -0400

    array of array

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+const skills = [
+  ['HTML', 10],
+  ['CSS', 7],
+  ['JavaScript', 9],
+  ['React', 8],
+]
+
+// Skill Component
+const Skill = ({ skill: [tech, level] }) => (
+  <li>
+    {tech} {level}
+  </li>
+)
+
+// Skills Component
+const Skills = ({ skills }) => {
+  const skillsList = skills.map((skill) => <Skill skill={skill} />)
+  console.log(skillsList)
+  return <ul>{skillsList}</ul>
+}
+
 const App = () => {
   return (
     <div className='container'>
       <div>
-        <h1>Numbers List</h1>
-        {[1, 2, 3, 4, 5]}
+        <h1>Skills Level</h1>
+        <Skills skills={skills} />
       </div>
     </div>
   )

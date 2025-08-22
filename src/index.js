013abc90dbc9787fbf668a6ccef28c397f333544
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-22 11:21:16 -0400

    array of objects

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const skills = [
-  ['HTML', 10],
-  ['CSS', 7],
-  ['JavaScript', 9],
-  ['React', 8],
+const countries = [
+  { name: 'Finland', city: 'Helsinki' },
+  { name: 'Sweden', city: 'Stockholm' },
+  { name: 'Denmark', city: 'Copenhagen' },
+  { name: 'Norway', city: 'Oslo' },
+  { name: 'Iceland', city: 'Reykjavík' },
 ]
 
-// Skill Component
-const Skill = ({ skill: [tech, level] }) => (
-  <li>
-    {tech} {level}
-  </li>
-)
-
-// Skills Component
-const Skills = ({ skills }) => {
-  const skillsList = skills.map((skill) => <Skill skill={skill} />)
-  console.log(skillsList)
-  return <ul>{skillsList}</ul>
-}
-
-const App = () => {
+// Country component
+const Country = ({ country: { name, city } }) => {
   return (
-    <div className='container'>
-      <div>
-        <h1>Skills Level</h1>
-        <Skills skills={skills} />
-      </div>
+    <div>
+      <h1>{name}</h1>
+      <small>{city}</small>
     </div>
   )
 }
+
+// countries component
+const Countries = ({ countries }) => {
+  const countryList = countries.map((country) => <Country country={country} />)
+  return <div>{countryList}</div>
+}
+// App component
+const App = () => (
+  <div className='container'>
+    <div>
+      <h1>Countries List</h1>
+      <Countries countries={countries} />
+    </div>
+  </div>
+)
 
 const rootElement = document.getElementById('root')
 

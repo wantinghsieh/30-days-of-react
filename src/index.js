3ea0898315ae283252d8f7ae364f5b14f81136c9
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-22 08:57:47 -0400

    props

--- src/index.js
+++ src/index.js
@@ -1,36 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// Header Component
-const Header = (props) => {
-  console.log(props)
-  return (
-    <header>
-      <div className='header-wrapper'>
-        <h1>{props.welcome}</h1>
-        <h2>{props.title}</h2>
-        <h3>{props.subtitle}</h3>
-        <p>
-          {props.firstName} {props.lastName}
-        </p>
-        <small>{props.date}</small>
-      </div>
-    </header>
-  )
-}
+const Skills = (props) => <ul>{props.skills}</ul>
 
-// The App, or the parent or the container component
-// Functional Component
 const App = () => (
   <div className='app'>
-    <Header
-      welcome='Welcome to 30 Days Of React'
-      title='Getting Started React'
-      subtitle='JavaScript Library'
-      firstName='Asabeneh'
-      lastName='Yetayeh'
-      date='Oct 4, 2020'
-    />
+    <Skills skills={['HTML', 'CSS', 'JavaScript']} />
   </div>
 )
 

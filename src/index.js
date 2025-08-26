b09eec6b229cc5f3f500db7c296fc53ec0c14979
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-26 15:56:49 -0400

    React Router DOM

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+
 // Home component
 const Home = (props) => <h1>Welcome Home</h1>
+// About component
+const About = (props) => <h1>About Us</h1>
+// Contact component
+const Contact = (props) => <h1>Contact us</h1>
+// Challenge component
+const Challenges = (props) => (
+  <div>
+    <h1>30 Days Of React Challenge</h1>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className='App'>
-          <Routes>
-            <Route path='/home' element={<Home />} />
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/challenges' element={<Challenges />} />
           </Routes>
         </div>
       </BrowserRouter>

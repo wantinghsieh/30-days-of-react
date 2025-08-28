b38762930681f079fbbc7f307f1572060803061d
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-28 10:36:35 -0400

    Getting data from an input field

--- src/index.js
+++ src/index.js
@@ -1,35 +1,29 @@
-// index.js
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
-const App = () => {
-  // declaring state
-  const url =
-    'https://www.smithsstationah.com/imagebank/eVetSites/Feline/01.jpg'
 
-  const [image, setImage] = useState(url)
-
-  const changeAnimal = () => {
-    let dogURL =
-      'https://static.onecms.io/wp-content/uploads/sites/12/2015/04/dogs-pembroke-welsh-corgi-400x400.jpg'
-    let catURL =
-      'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg'
-    let result = image === catURL ? dogURL : catURL
-    setImage(result)
+const App = (props) => {
+  // initial state and method to update state
+  const [firstName, setFirstName] = useState('')
+  const handleChange = (e) => {
+    const value = e.target.value
+    setFirstName(value)
   }
-
   return (
     <div className='App'>
-      <h1>30 Days Of React</h1>
-      <div className='animal'>
-        <img src={image} alt='animal' />
-      </div>
-
-      <button onClick={changeAnimal} class='btn btn-add'>
-        Change
-      </button>
+      <label htmlFor='firstName'>First Name: </label>
+      <input
+        type='text'
+        id='firstName'
+        name='firstName'
+        placeholder='First Name'
+        value={firstName}
+        onChange={handleChange}
+      />
+      <h1>{firstName}</h1>
     </div>
   )
 }
+
 const rootElement = document.getElementById('root')
 // create a root
 const root = ReactDOM.createRoot(rootElement)

de96f32ef7c36a129e7c1d7c8285b8f44c198fe3
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-28 10:26:44 -0400

    useState

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 // index.js
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
-
 const App = () => {
-  // Declaring new state variable
-  const [count, setCount] = useState(0)
-  const addOne = () => {
-    let value = count + 1
-    setCount(value)
+  // declaring state
+  const url =
+    'https://www.smithsstationah.com/imagebank/eVetSites/Feline/01.jpg'
+
+  const [image, setImage] = useState(url)
+
+  const changeAnimal = () => {
+    let dogURL =
+      'https://static.onecms.io/wp-content/uploads/sites/12/2015/04/dogs-pembroke-welsh-corgi-400x400.jpg'
+    let catURL =
+      'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg'
+    let result = image === catURL ? dogURL : catURL
+    setImage(result)
   }
-  const minusOne = () => {
-    let value = count - 1
-    setCount(value)
-  }
+
   return (
     <div className='App'>
-      <h1>{count} </h1>
-      <button onClick={addOne}>Add One</button> <button onClick={minusOne}>Minus One</button>
+      <h1>30 Days Of React</h1>
+      <div className='animal'>
+        <img src={image} alt='animal' />
+      </div>
+
+      <button onClick={changeAnimal} class='btn btn-add'>
+        Change
+      </button>
     </div>
   )
 }

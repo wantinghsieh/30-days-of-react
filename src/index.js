6063b7bdb078035087186bfc957ad0b87c6c7a0f
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-28 10:45:51 -0400

    Getting multiple input data from form

--- src/index.js
+++ src/index.js
@@ -2,24 +2,78 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
 const App = (props) => {
-  // initial state and method to update state
-  const [firstName, setFirstName] = useState('')
-  const handleChange = (e) => {
-    const value = e.target.value
-    setFirstName(value)
+  const initialState = {
+    firstName: '',
+    lastName: '',
+    country: '',
+    title: '',
   }
+  const [formData, setData] = useState(initialState)
+
+  const onChange = (e) => {
+    const { name, value } = e.target
+    setData({ ...formData, [name]: value })
+  }
+  const onSubmit = (e) => {
+    /* 
+     e.preventDefault()
+      stops the default behavior of form element
+     specifically refreshing of page
+     */
+    e.preventDefault()
+
+    /*
+     the is the place where we connect backend api 
+     to send the data to the database
+     */
+    console.log(formData)
+  }
+
+  // accessing the state value by destrutcturing the state
+  const { firstName, lastName, title, country } = formData
   return (
     <div className='App'>
-      <label htmlFor='firstName'>First Name: </label>
-      <input
-        type='text'
-        id='firstName'
-        name='firstName'
-        placeholder='First Name'
-        value={firstName}
-        onChange={handleChange}
-      />
-      <h1>{firstName}</h1>
+      <h3>Add Student</h3>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            type='text'
+            name='firstName'
+            placeholder='First Name'
+            value={firstName}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <input
+            type='text'
+            name='lastName'
+            placeholder='Last Name'
+            value={lastName}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <input
+            type='text'
+            name='country'
+            placeholder='Country'
+            value={country}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <input
+            type='text'
+            name='title'
+            placeholder='Title'
+            value={title}
+            onChange={onChange}
+          />
+        </div>
+
+        <button className='btn btn-success'>Submit</button>
+      </form>
     </div>
   )
 }

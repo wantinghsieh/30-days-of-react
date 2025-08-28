7abab88d21b7868635048d9002be0e69204b1e24
wanting hsieh <Wanting.Hsieh@cox.com>
2025-08-28 10:13:44 -0400

    useState

--- src/index.js
+++ src/index.js
@@ -1,38 +1,18 @@
-import React, { Component } from 'react'
+// index.js
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+const App = () => {
+  // Declaring new state variable
+  const [count, setCount] = useState(0)
 
-// Home component
-const Home = (props) => <h1>Welcome Home</h1>
-// About component
-const About = (props) => <h1>About Us</h1>
-// Contact component
-const Contact = (props) => <h1>Contact us</h1>
-// Challenge component
-const Challenges = (props) => (
-  <div>
-    <h1>30 Days Of React Challenge</h1>
-  </div>
-)
-
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className='App'>
-        <Routes>
-          <Route path='/home' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/challenges' element={<Challenges />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    )
-  }
+  return (
+    <div className='App'>
+      <h1>{count} </h1>
+      <button onClick={() => setCount(count + 1)}>Add One</button>
+    </div>
+  )
 }
-
 const rootElement = document.getElementById('root')
 // create a root
 const root = ReactDOM.createRoot(rootElement)
